7f9bd3f1fea099ab6ef142f2a95ffce60355fbdd
Michal Weinert <michal@weinert.io>
2023-08-17 11:58:30 +0200

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, json, Response, request
+from random import choice
+from fuzzywuzzy import fuzz, process
+
+app = Flask(__name__)
+
+excuses = []
+with open("excuses.txt") as file:
+    for line in file:
+        excuses.append(line.strip('\n'))
+
+@app.route('/bofh', methods = ['GET', 'POST'])
+def bofh():
+    if "text" in request.form and request.form["text"]:
+        quotes = process.extract(request.form["text"], excuses, limit=5)
+        quote = choice(quotes)[0]
+    else:
+        quote = choice(excuses)
+
+    response = json.dumps({"response_type": "in_channel", "text": quote})
+    return Response(response, mimetype="application/json")
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80, app, use_reloader=False, use_debugger=False, use_evalex=False)
